e7f949d3d2275f1728c66ca3c643892cb0900108
SulthanYasin <sulthanzain28@gmail.com>
2024-05-11 21:51:20 +0700

    UI speedtest fix

--- canvas.js
+++ canvas.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+  let waktuBelajarElement = document.getElementById("waktu-belajar");
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
 
   var x = canvas.width / 2;
-  var y = canvas.height / 2;
+  var y = canvas.height;
 
   function drawgradient() {
     ctx.translate(x, y);
-    ctx.rotate((0 * Math.PI) / 180);
     ctx.translate(-x, -y);
+    ctx.rotate((0 * Math.PI) / 180);
 
     var gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
     gradient.addColorStop(0, "rgba(0,255,68,1)");
@@ -16,21 +17,20 @@ document.addEventListener("DOMContentLoaded", function () {
     gradient.addColorStop(1, "rgba(28,28,157,1)");
 
     ctx.strokeStyle = gradient;
-    ctx.lineWidth = 15;
+    ctx.lineWidth = 25;
     ctx.beginPath();
-    ctx.arc(x, y, 120, 195 * (Math.PI / 180), 345 * (Math.PI / 180), false);
+    ctx.arc(x - 1, y, 120, 195 * (Math.PI / 180), 344 * (Math.PI / 180), false);
     ctx.stroke();
   }
   function drawSolid() {
     ctx.translate(x, y);
-
     ctx.translate(-x, -y);
 
     ctx.strokeStyle = "#3f3f46";
-    ctx.lineWidth = 17; //a
+    ctx.lineWidth = 27; //a
     ctx.beginPath();
     ctx.arc(
-      x,
+      x - 1,
       y,
       120,
       195 + angle * (Math.PI / 180),
@@ -40,34 +40,34 @@ document.addEventListener("DOMContentLoaded", function () {
     ctx.stroke();
   }
 
-  let angle = 185; // 185 == 0 MAX 332.3
+  let angle = 180; // 180 == 0 MAX 332.3
   let intervalId;
 
   function addtext() {
     ctx.font = "15px sans-serif";
     ctx.fillStyle = "white";
-    ctx.fillText("0", 55, 120);
+    ctx.fillText("0", 60, 120);
 
     ctx.font = "15px sans-serif";
     ctx.fillStyle = "white";
-    ctx.fillText("0", 235, 120);
+    ctx.fillText(waktuBelajarElement.innerText, 220, 120);
   }
-  ctx.font = "30px Arial"; // Atur ukuran dan jenis font teks
-  ctx.fillStyle = "black"; // Atur warna teks
-  ctx.fillText("Hello, World!", 50, 50); // Tambahkan teks di koordinat (50, 50)
 
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawgradient();
     addtext();
-
     if (angle < 332) {
-      angle++;
+      angle += 0.1;
     } else {
       clearInterval(intervalId);
     }
     drawSolid(angle);
+    console.log(angle);
   }
+  drawgradient();
+  addtext();
+  drawSolid(angle);
 
   intervalId = setInterval(animate, 10);
 });
